3f23c7ffa3406bfd9e1b29dc88d5859607b3a945
Tanwim Ahmed Chowdhury Nafi <140414569+Tanwim@users.noreply.github.com>
2026-06-09 16:12:19 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,22 @@
+text = input().strip()
+k = int(input())
 
+counts ={}
+
+for i in range(len(text)-k+1):
+  pattern =text[i:i+k]
+
+  if pattern in counts:
+    counts[pattern] += 1
+  else:
+    counts[pattern] = 1
+
+max_count = max(counts.values())
+
+result =[]
+
+for pattern in counts:
+  if counts[pattern] == max_count:
+    result.append(pattern)
+
+print(result)
